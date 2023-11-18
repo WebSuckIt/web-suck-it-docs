273bd04bcff8748b0b57337899fdfbc8b0c70b4f
Diretnan Domnan <diretnandomnan@gmail.com>
2023-11-18 22:45:38 +0100

    Remove docusaurus as social card

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -92,7 +92,7 @@ const config = {
         respectPrefersColorScheme: false,
       },
       // Replace with your project's social card
-      image: "img/docusaurus-social-card.jpg",
+      image: "img/Logo-light.png",
       navbar: {
         // title: "Websuckit",
         logo: {
