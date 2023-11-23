57f7a0fbe740aefa8d09caefb9cc7c418e028fb6
JoshX <joshx.dev@gmail.com>
2023-11-23 14:01:37 +0100

    docs: replace sidebar icons

---
+++ src/Card.tsx
@@ -0,0 +1,34 @@
+import React, { ReactNode } from "react";
+
+type CardProps = {
+ title: string;
+ description: string;
+ icon?: ReactNode;
+ href?: string;
+};
+const Card = ({
+ title = "websuckit",
+ description = "websuckit",
+ icon,
+ href = "#"
+}: CardProps) => {
+ return (
+  <article>
+   <a href={href} style={{ textDecoration: "none" }}>
+    <div className="card">
+     <div className="text--truncate">
+      <div style={{ display: "flex", alignItems: "self-end", gap: "8px" }}>
+       {icon ? icon : ""}
+       <h2 className="text--truncate" title={title}>
+        {title}
+       </h2>
+      </div>
+     </div>
+     <p className="text--truncate">{description}</p>
+    </div>
+   </a>
+  </article>
+ );
+};
+
+export default Card;
