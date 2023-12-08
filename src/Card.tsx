d8db9e28368f774e0075cdbae0b40cfcdff7b36d
DocBot <>
2023-12-08 08:29:13 +0000

    Update from https://github.com/WebSuckIt/web-suck-it-docs/commit/e511a124c3f9b0b860b6347948bf2e4401de95d3

--- src/Card.tsx
+++ src/Card.tsx
@@ -17,14 +17,21 @@ const Card = ({
    <a href={href} style={{ textDecoration: "none" }}>
     <div className="card">
      <div className="text--truncate">
-      <div style={{ display: "flex", alignItems: "self-end", gap: "8px" }}>
+      <div
+       style={{
+        display: "flex",
+        alignItems: "center",
+        gap: "8px",
+        marginBottom: "16px"
+       }}
+      >
        {icon ? <span title={title}>{icon}</span> : ""}
        <h2 className="text--truncate" title={title}>
         {title}
        </h2>
       </div>
      </div>
-     <p className="text--truncate">{description}</p>
+     <p className="text--truncate" title={description}>{description}</p>
     </div>
    </a>
   </article>
